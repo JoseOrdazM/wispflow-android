73f462b241d0d386b6d4e1cdd6af01bef2b3ce41
Jose Ordaz <59866823+JoseOrdazM@users.noreply.github.com>
2026-05-28 20:50:20 +0200

    fix: overlay.dart v1.4

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,72 +1,69 @@
+import "dart:async";
 import "package:flutter/material.dart";
-import "services/overlay_service.dart";
+import "package:flutter_overlay_window/flutter_overlay_window.dart";
 
 @pragma("vm:entry-point")
 void overlayMain() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: FloatingBubble(),
+    home: OverlayBubble(),
   ));
 }
 
-class FloatingBubble extends StatefulWidget {
-  const FloatingBubble({super.key});
-
+class OverlayBubble extends StatefulWidget {
+  const OverlayBubble({super.key});
   @override
-  State<FloatingBubble> createState() => _FloatingBubbleState();
+  State<OverlayBubble> createState() => _OverlayBubbleState();
 }
 
-class _FloatingBubbleState extends State<FloatingBubble> {
-  final OverlayService _service = OverlayService();
-  BubbleState _state = BubbleState.idle;
+enum _BubbleState { idle, recording, processing }
+
+class _OverlayBubbleState extends State<OverlayBubble> {
+  _BubbleState _state = _BubbleState.idle;
+  StreamSubscription? _sub;
 
   @override
   void initState() {
     super.initState();
-    _service.stateStream.listen((s) {
-      if (mounted) setState(() => _state = s);
+    _sub = FlutterOverlayWindow.overlayListener.listen((data) {
+      if (data is String) {
+        if (mounted) {
+          setState(() {
+            if (data == "state:recording") _state = _BubbleState.recording;
+            else if (data == "state:processing") _state = _BubbleState.processing;
+            else if (data == "state:idle") _state = _BubbleState.idle;
+          });
+        }
+      }
     });
   }
 
-  Color _bubbleColor() {
+  @override
+  void dispose() {
+    _sub?.cancel();
+    super.dispose();
+  }
+
+  Color get _color {
     switch (_state) {
-      case BubbleState.idle:
-        return Colors.deepPurple;
-      case BubbleState.recording:
-        return Colors.redAccent;
-      case BubbleState.processing:
-        return Colors.orange;
+      case _BubbleState.idle: return Colors.deepPurple;
+      case _BubbleState.recording: return Colors.red;
+      case _BubbleState.processing: return Colors.orange;
     }
   }
 
-  Widget _bubbleContent() {
+  Widget get _icon {
     switch (_state) {
-      case BubbleState.idle:
-        return const Icon(Icons.mic, color: Colors.white, size: 28);
-      case BubbleState.recording:
-        return const Stack(
-          alignment: Alignment.center,
-          children: [
-            Icon(Icons.stop, color: Colors.white, size: 22),
-            SizedBox(
-              width: 44,
-              height: 44,
-              child: CircularProgressIndicator(
-                strokeWidth: 2,
-                valueColor: AlwaysStoppedAnimation<Color>(Colors.white70),
-              ),
-            ),
-          ],
-        );
-      case BubbleState.processing:
+      case _BubbleState.idle:
+        return const Icon(Icons.mic, color: Colors.white, size: 30);
+      case _BubbleState.recording:
+        return const Icon(Icons.stop, color: Colors.white, size: 28);
+      case _BubbleState.processing:
         return const SizedBox(
-          width: 28,
-          height: 28,
-          child: CircularProgressIndicator(
-            strokeWidth: 3,
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-          ),
+          width: 26, height: 26,
+          child: CircularProgressIndicator(strokeWidth: 3,
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white)),
         );
     }
   }
@@ -74,22 +71,27 @@ class _FloatingBubbleState extends State<FloatingBubble> {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () => _service.onBubbleTap(),
-      child: Container(
-        width: 60,
-        height: 60,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: _bubbleColor(),
-          boxShadow: [
-            BoxShadow(
-              color: _bubbleColor().withOpacity(0.4),
-              blurRadius: 12,
-              spreadRadius: 2,
-            ),
-          ],
+      onTap: () {
+        FlutterOverlayWindow.shareData("bubble:tap");
+      },
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          width: 62,
+          height: 62,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: _color,
+            boxShadow: [
+              BoxShadow(
+                color: _color.withOpacity(0.5),
+                blurRadius: 14,
+                spreadRadius: 3,
+              ),
+            ],
+          ),
+          child: Center(child: _icon),
         ),
-        child: _bubbleContent(),
       ),
     );
   }
