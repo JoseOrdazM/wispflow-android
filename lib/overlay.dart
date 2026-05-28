d19cb7bd1c55133a1c930735c802058457658317
Jose Ordaz <59866823+JoseOrdazM@users.noreply.github.com>
2026-05-28 11:22:22 +0200

    fix: overlay entry point @pragma + overlayMain()

--- lib/overlay.dart
+++ lib/overlay.dart
@@ -1,7 +1,8 @@
 import "package:flutter/material.dart";
 import "services/overlay_service.dart";
 
-void main() {
+@pragma("vm:entry-point")
+void overlayMain() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
