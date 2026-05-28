8ac98cd098ba755f65aff40214a87d375b704181
Jose Ordaz <59866823+JoseOrdazM@users.noreply.github.com>
2026-05-28 12:14:44 +0200

    Fix: actualizar config para Flutter 3.24.5 + Java 17 + Gradle 8.7 + AGP 8.4.0

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -3,8 +3,8 @@ pluginManagement {
         run {
             val properties = java.util.Properties()
             file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            val flutterSkkPath = properties.getProperty("flutter.sdk")
+            require(flutterSkkPath != null) { "flutter.sdk not set in local.properties" }
             flutterSdkPath
         }
 
@@ -19,7 +19,7 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application") version "8.4.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
 }
 
